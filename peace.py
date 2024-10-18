73b47ee80b875d113e99c53d4abec6dc4ebf94db
Zaccaria Broomhall Bedont <zaccariamontreal@gmail.com>
2024-10-18 13:38:01 -0400

    bugfixes and print statements start

--- peace.py
+++ peace.py
@@ -19,16 +19,17 @@ def card_comparison(p1_card, p2_card):
 def play_round(player1_hand, player2_hand):
 
     battle_result = card_comparison(player1_hand[0],player2_hand[0])
+    print(f"Player 1 played {player1_hand[0]} and Player 2 played {player2_hand[0]}")
+    temp_deck = [player1_hand.pop(0),player2_hand.pop(0)]
 
     if battle_result == 1:
-        temp_deck = [player1_hand.pop(0),player2_hand.pop(0)]
-        player1_hand.extend(temp_deck)
+        print("Player 1 beat Player 2 and took their cards")
         return player1_hand, player2_hand
     elif battle_result == 2:
-        temp_deck = [player1_hand.pop(0),player2_hand.pop(0)]
-        player2_hand.extend(temp_deck)
+        print("Player 2 beat Player 1 and took their cards")
         return player1_hand,player2_hand
     else:
+        print("Looks like we're going to war")
         player1_hand,player2_hand = peace(player1_hand,player2_hand)
         return player1_hand,player2_hand
 
@@ -37,10 +38,10 @@ def peace(player1_hand, player2_hand):
 
     while len(player1_hand) > 3 and len(player2_hand)> 3:
 
-        temp_deck.extend(player1_hand[:3])
-        temp_deck.extend(player2_hand[:3])
-        del player1_hand[:3]
-        del player2_hand[:3]
+        temp_deck +=  [player1_hand.pop(0) for _ in range(3)]
+        temp_deck += [player2_hand.pop(0) for _ in range(3)]
+        print("Both players have given up three cards")
+        print(f"Player 1 brings {player1_hand[0]} and Player 2 brings {player2_hand} to war")
         peace_result = card_comparison(player1_hand[0],player2_hand[0])
         temp_deck.append(player1_hand.pop(0))
         temp_deck.append(player2_hand.pop(0))
@@ -53,10 +54,8 @@ def peace(player1_hand, player2_hand):
             return player1_hand,player2_hand
 
     if len(player1_hand) > 0 and len(player2_hand) > 0:
-        temp_deck.extend(player1_hand[:-1])
-        temp_deck.extend(player2_hand[:-1])
-        del player1_hand[:-1]
-        del player2_hand[:-1]
+        temp_deck += [player1_hand.pop(0) for _ in range(len(player1_hand)-1)]
+        temp_deck += [player2_hand.pop(0) for _ in range(len(player2_hand)-1)]
         peace_result = card_comparison(player1_hand[0],player2_hand[0])
         temp_deck.append(player1_hand.pop(0))
         temp_deck.append(player2_hand.pop(0))
@@ -68,24 +67,20 @@ def peace(player1_hand, player2_hand):
         elif peace_result == 2:
             player2_hand.extend(temp_deck)
             return player1_hand,player2_hand
-    else:
-        if len(player1_hand) == 0 and player2_hand != 0:
-            return "Player 2 wins!"
-        elif len(player2_hand) == 0 and player1_hand != 0:
-            return "Player 1 wins!"
         else:
-            return "It's a tie!"
+            return player1_hand,player2_hand
+    else:
+        return player1_hand,player2_hand
 
         
 def play_game(deck1,deck2):
-    while len(deck1) > 0 and len(deck2) > 0 and deck1 != "Player 1 wins!" and deck1 != "Player 2 wins!" and deck1 != "It's a tie!":
+    while len(deck1) > 0 and len(deck2) > 0:
         deck1,deck2 = play_round(deck1,deck2)
-        print(deck1,deck2)
-    if deck1 == "it's a tie!" or deck1 == "Player 2 wins!" or deck1 == "Player 1 wins!":
-        print(deck1)
-    elif len(deck1) == 0:
+    if len(deck1) == 0:
         print("Player 2 wins!")
-    else:
+    elif len(deck2) == 0:
         print("Player 1 wins")
+    else:
+        print("It's a tie!")
 
 play_game(deck1,deck2)
